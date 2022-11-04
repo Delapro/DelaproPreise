96e24d062ecac5f0f38cbea9b8998fd1a54f513e
easy <Delapro@users.noreply.github.com>
2022-11-04 16:17:23 +0100

    Update VDDSPreiseTesten.ps1

--- Tests/VDDSPreiseTesten.ps1
+++ Tests/VDDSPreiseTesten.ps1
@@ -18,5 +18,5 @@ $k.kzv|select Kurzname, kzvnummer, @{N='CSVName';E={$_.PreisCSVLink.Segments[-1]
 # eine bestimmte KZV auswählen
 $kzv = $k.kzv|where name -eq "Thüringen"
 
-"$PSHost.Ui.RawUI:"
-$pshost.ui.rawui
+"(Get-Host).Ui.RawUI:"
+(Get-host).ui.RawUI

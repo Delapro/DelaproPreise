8a21f55d4a7506a78dc138b5229dccc823e82345
easy <Delapro@users.noreply.github.com>
2022-11-04 16:00:53 +0100

    Überschriften hinzugefügt

--- Tests/VDDSPreiseTesten.ps1
+++ Tests/VDDSPreiseTesten.ps1
@@ -1,8 +1,12 @@
 # Anwendung
 $k=[kzven]::new()
+"Nach Kzv-Nummer sortiert:"
 $k.kzv|select kzvnummer, Name, Homepage | sort-object kzvnummer
+"Nach Name sortiert:"
 $k.kzv|select kzvnummer, Name, Homepage | sort-object Name
+"Nach Kurzname sortiert:"
 $k.kzv|select Kurzname, PreisCSVLink | sort-object Kurzname
+"Nach Kurzname sortiert mit CSV-Link:"
 $k.kzv|select Kurzname, kzvnummer, @{N='CSVName';E={$_.PreisCSVLink.Segments[-1]}} | sort-object Kurzname
 
 # Preise können generell von KZV, Innung, oder Softwarehersteller kommen

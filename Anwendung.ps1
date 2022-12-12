8bc065cb7caf5af571daeaf2bd7dbcd3f5614923
easy <Delapro@users.noreply.github.com>
2022-12-12 01:01:03 +0100

    GetCSVName() dokumentiert

--- Anwendung.ps1
+++ Anwendung.ps1
@@ -3,7 +3,9 @@ $k=[kzven]::new()
 $k.kzv|select kzvnummer, Name, Homepage | sort kzvnummer
 $k.kzv|select kzvnummer, Name, Homepage | sort Name
 $k.kzv|select Kurzname, PreisCSVLink | sort Kurzname
-$k.kzv|select Kurzname, kzvnummer, @{N='CSVName';E={$_.PreisCSVLink.Segments[-1]}} | sort Kurzname
+# $k.kzv|select Kurzname, kzvnummer, @{N='CSVName';E={$_.PreisCSVLink.Segments[-1]}} | sort Kurzname
+# bessere Variante die CSV-Dateinamen zu ermitteln, wegen indirekten Links:
+$k.kzv|select Kurzname, kzvnummer, @{N='CSVName';E={$_.GetCSVName()}} | sort Kurzname
 
 # Preise können generell von KZV, Innung, oder Softwarehersteller kommen
 # in der Regel in der Form CSV-, PDF- oder Excel-Datei

865c1f9af5de9924f1b49bcd1fb07c1dbba4f9e7
easy innovative software <Delapro@users.noreply.github.com>
2023-11-03 14:14:14 +0100

    Ausgabe der Preise Homepage

--- Tests/VDDSPreiseTesten.ps1
+++ Tests/VDDSPreiseTesten.ps1
@@ -8,6 +8,8 @@ $k.kzv|select kzvnummer, Name, Homepage | sort-object Name
 $k.kzv|select Kurzname, PreisCSVLink | sort-object Kurzname |fl
 "Nach Kurzname sortiert mit CSV-Dateiname:"
 $k.kzv|select Kurzname, kzvnummer, @{N='CSVName';E={$_.PreisCSVLink.Segments[-1]}} | sort-object Kurzname |fl
+"Ausgabe der Preisseiten, nach Name sortiert:"
+$k.kzv|select kzvnummer, Name, HomepagePreise | sort-object Name
 
 # Preise können generell von KZV, Innung, oder Softwarehersteller kommen
 # in der Regel in der Form CSV-, PDF- oder Excel-Datei
